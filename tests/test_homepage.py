21d92ac01720244428ba981f28c21ef199a95892
Eric Sammons <esammons@redhat.com>
2012-01-10 08:46:30 -0500

    Working page load. now to build an actual test.

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python 
 
 import pytest
+
 from unittestzero import Assert
-import xmlrunner
-import sys
 from pages.home import Home
 
 xfail = pytest.mark.xfail
 nondestructive = pytest.mark.nondestructive
 
 class TestHome:
+    
+    @nondestructive
+    def test_Title(self, mozwebqa):
+        home_page = Home(mozwebqa)
         
-    def test_Title(self):
-        home_page = Home(self.base_url)
-        
-    @nondestructive  
-    def tearDown(self):
-        self.driver.close()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='../junitreport'))

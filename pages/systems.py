150812b4b7bc37bc1e78f999ed77ad228ea594a2
Eric Sammons <esammons@redhat.com>
2012-01-19 20:41:13 -0500

    Modified timeout values for webdriverwait

--- pages/systems.py
+++ pages/systems.py
@@ -56,7 +56,7 @@ class SystemsTab(Base):
         ActionChains(self.selenium).move_to_element(save_button_locator).\
             click().perform()
 
-        WebDriverWait(self.selenium, 120).until(lambda s: self.is_element_present(*self._system_block_active_locator))
+        WebDriverWait(self.selenium, 30).until(lambda s: self.is_element_present(*self._system_block_active_locator))
         
     def remove_a_system(self):
         '''
@@ -75,8 +75,8 @@ class SystemsTab(Base):
         ActionChains(self.selenium).move_to_element(confirm_button_locator).\
             click().perform()
         
-        WebDriverWait(self.selenium, 120).until(lambda s: self.is_element_present(*self._system_list_locator))
-        WebDriverWait(self.selenium, 60).until(lambda s: len(self.systems) < current_no_systems)
+        WebDriverWait(self.selenium, 30).until(lambda s: self.is_element_present(*self._system_list_locator))
+        WebDriverWait(self.selenium, 30).until(lambda s: len(self.systems) < current_no_systems)
     
     @property
     def is_system_facts_tab_present(self):

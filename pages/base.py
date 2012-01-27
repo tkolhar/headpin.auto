05a9d4b8e4c02f5423dc9c623431bc5c51151fa3
Eric Sammons <esammons@redhat.com>
2012-01-27 16:22:39 -0500

    Still having issues with the rate at which block refreshes. Trying with a send RETURN "\n"

--- pages/base.py
+++ pages/base.py
@@ -52,10 +52,10 @@ class Base(Page):
     
     def enter_search_criteria(self, criteria):
         search_input_locator = self.selenium.find_element(*self._search_input_locator)
-        search_input_locator.send_keys(criteria)
+        search_input_locator.send_keys(criteria+"\n")
         self.selenium.find_element(*self._search_button_locator).click()
         # Give block time to update; should investigate using WebDriverWait.
-        time.sleep(3)
+        time.sleep(5)
 
     @property
     def redhat_logo_image_source(self):

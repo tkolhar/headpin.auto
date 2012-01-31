9aac6b67643f8d29495f03d4addb3f2223498899
Eric Sammons <esammons@redhat.com>
2012-01-31 10:53:53 -0500

    Added a create_org call to test_switch_org to ensure that we have a org, just in case.  We need at least 2 for this to work.

--- tests/test_contentmgmt.py
+++ tests/test_contentmgmt.py
@@ -4,6 +4,7 @@ import pytest
 
 from unittestzero import Assert
 from pages.home import Home
+from pages.api import apiTasks
 import time
 xfail = pytest.mark.xfail
 
@@ -13,7 +14,17 @@ class TestContentManagement:
         home_page = Home(mozwebqa)
         home_page.login()
         Assert.true(home_page.is_successful)
-        
+        ####
+        # Create a new org, have to ensure we have +1
+        # Don't fear if this org is not the selected.
+        ###
+        sysapi = apiTasks(mozwebqa)
+        new_org_name = "someorg-%s" % home_page.random_string()
+        sysapi.create_org(new_org_name)
+        ###
+        # Get the current org for comparison later
+        # and then switch org
+        ###        
         current_org = home_page.header.get_text_from_switcher()
         home_page.header.click_switcher()
         home_page.header.click_org_from_switcher()

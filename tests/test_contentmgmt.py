d2ad90630e954b09555b2132afd5c56129c937de
Eric Sammons <esammons@redhat.com>
2012-02-06 15:19:18 -0500

    Updated test_contentmgmt to address all current scenarios discussed.
    TODO:
    - See about creating unique ad-hoc manifests.

--- tests/test_contentmgmt.py
+++ tests/test_contentmgmt.py
@@ -10,6 +10,11 @@ import time
 xfail = pytest.mark.xfail
 
 class TestContentManagement:
+    _org1_m1_manifest = "/var/tmp/manifest_D1_O1_M1.zip"
+    _org2_m1_manifest = "/var/tmp/manifest_D2_O2_M1.zip"
+    _org3_m1_manifest = "/var/tmp/manifest_D3_O3_M1.zip"
+    _org4_m1_manifest = "/var/tmp/manifest_D4_O4_M1.zip"
+    _org1_m2_manifest = "/var/tmp/manifest_D1_O1_M2.zip"
     
     def test_switch_org(self, mozwebqa):
         home_page = Home(mozwebqa)
@@ -32,7 +37,11 @@ class TestContentManagement:
         active_org = home_page.header.get_text_from_switcher()
         Assert.not_equal(active_org, current_org)
             
-    def test_load_manifest(self, mozwebqa, manifest_file="/var/tmp/sam_manifest_1.zip"):
+    def test_load_manifest(self, mozwebqa):
+        '''
+        Scenario 1: Import Manifest (M1) from Distributor (D1) input Org1
+        Result: Pass
+        '''
         home_page = Home(mozwebqa)
         home_page.login()
         Assert.true(home_page.is_successful)
@@ -54,14 +63,145 @@ class TestContentManagement:
         cm = ContentManagementTab(mozwebqa)
         home_page.tabs.click_tab("content_management_tab")
         Assert.true(home_page.is_the_current_page)
-        cm.enter_manifest(manifest_file)
+        cm.enter_manifest(self._org1_m1_manifest)
         Assert.true(home_page.is_successful)
         Assert.not_equal(cm.get_content_table_text, "No subscriptions have been imported.")
         
-    def test_load_second_manifest(self, mozwebqa):
-        self.test_load_manifest(mozwebqa, "/var/tmp/sam_manifest_2.zip")
+    def test_load_same_manifest_to_same_org_wo_force(self, mozwebqa):
+        '''
+        Scenario 2 with a twist. Import Manifest (M1) from Distributor (D1) into Org1 w/o useing force.
+        Result: Expect Fail
+        '''
+        home_page = Home(mozwebqa)
+        home_page.login()
+        Assert.true(home_page.is_successful)
+        ###
+        # Create a org to work with
+        ###
+        sysapi = apiTasks(mozwebqa)
+        new_org_name = "manifest-%s" % home_page.random_string()
+        sysapi.create_org(new_org_name)
+        ###
+        # Find that org and select it
+        ###
+        home_page.header.click_switcher()
+        home_page.header.filter_org_in_switcher(new_org_name)
+        home_page.header.click_filtered_result(new_org_name)
+        ###
+        # Install Manifest
+        ###
+        cm = ContentManagementTab(mozwebqa)
+        home_page.tabs.click_tab("content_management_tab")
+        Assert.true(home_page.is_the_current_page)
+        cm.enter_manifest(self._org3_m1_manifest)
+        Assert.true(home_page.is_successful)
+        ###
+        # Install manifest again
+        ###
+        cm.enter_manifest(self._org3_m1_manifest)
+        Assert.true(home_page.is_failed)
+    
+    def test_load_same_manifest_to_same_org_w_force(self, mozwebqa):
+        '''
+        Scenario 2: Re-import same manifest into same org.
+        Result: Pass
+        '''
+        home_page = Home(mozwebqa)
+        home_page.login()
+        Assert.true(home_page.is_successful)
+        ###
+        # Create a org to work with
+        ###
+        sysapi = apiTasks(mozwebqa)
+        new_org_name = "manifest-%s" % home_page.random_string()
+        sysapi.create_org(new_org_name)
+        ###
+        # Find that org and select it
+        ###
+        home_page.header.click_switcher()
+        home_page.header.filter_org_in_switcher(new_org_name)
+        home_page.header.click_filtered_result(new_org_name)
+        ###
+        # Install Manifest
+        ###
+        cm = ContentManagementTab(mozwebqa)
+        home_page.tabs.click_tab("content_management_tab")
+        Assert.true(home_page.is_the_current_page)
+        cm.enter_manifest(self._org4_m1_manifest)
+        Assert.true(home_page.is_successful)
+        ###
+        # Install manifest again
+        ###
+        cm.click_force()
+        cm.enter_manifest(self._org4_m1_manifest)
+        Assert.true(home_page.is_successful)
+        
+    def test_load_new_manifest_into_same_org_wo_force(self, mozwebqa):
+        '''
+        Scenario 5: Load updated (new) manifest into org where a manifest already exists.
+        Result: Pass
+        '''
+        home_page = Home(mozwebqa)
+        home_page.login()
+        Assert.true(home_page.is_successful)
+        ###
+        # Create a org to work with
+        ###
+        sysapi = apiTasks(mozwebqa)
+        new_org_name = "manifest-%s" % home_page.random_string()
+        sysapi.create_org(new_org_name)
+        ###
+        # Find that org and select it
+        ###
+        home_page.header.click_switcher()
+        home_page.header.filter_org_in_switcher(new_org_name)
+        home_page.header.click_filtered_result(new_org_name)
+        ###
+        # Install Manifest
+        ###
+        cm = ContentManagementTab(mozwebqa)
+        home_page.tabs.click_tab("content_management_tab")
+        Assert.true(home_page.is_the_current_page)
+        cm.enter_manifest(self._org1_m1_manifest)
+        Assert.true(home_page.is_successful)
+        ###
+        # Install manifest again
+        ###
+        cm.enter_manifest(self._org1_m2_manifest)
+        Assert.true(home_page.is_successful)
+        Assert.not_equal(cm.get_content_table_text, "No subscriptions have been imported.")
+        
+    def test_load_second_manifest_second_org(self, mozwebqa):
+        home_page = Home(mozwebqa)
+        home_page.login()
+        Assert.true(home_page.is_successful)
+        ###
+        # Create a org to work with
+        ###
+        sysapi = apiTasks(mozwebqa)
+        new_org_name = "manifest-%s" % home_page.random_string()
+        sysapi.create_org(new_org_name)
+        ###
+        # Find that org and select it
+        ###
+        home_page.header.click_switcher()
+        home_page.header.filter_org_in_switcher(new_org_name)
+        home_page.header.click_filtered_result(new_org_name)
+        ###
+        # Install Manifest
+        ###
+        cm = ContentManagementTab(mozwebqa)
+        home_page.tabs.click_tab("content_management_tab")
+        Assert.true(home_page.is_the_current_page)
+        cm.enter_manifest(self._org2_m1_manifest)
+        Assert.true(home_page.is_successful)
+        Assert.not_equal(cm.get_content_table_text, "No subscriptions have been imported.")
         
     def test_load_previous_manifest_to_another_org(self, mozwebqa):
+        '''
+        Scenario 3: Import Manifest (M1) from Distributor (D1) into Org2.
+        Result Expected: Fail
+        '''
         home_page = Home(mozwebqa)
         home_page.login()
         Assert.true(home_page.is_successful)
@@ -85,4 +225,4 @@ class TestContentManagement:
         Assert.true(home_page.is_the_current_page)
         cm.enter_manifest("/var/tmp/sam_manifest_1.zip")
         Assert.true(home_page.is_failed)
-        Assert.not_equal(cm.get_content_table_text, "No subscriptions have been imported.")
+        Assert.equal(cm.get_content_table_text, "No subscriptions have been imported.")

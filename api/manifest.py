fde5a4ce9a95820abe817058df7edcf71a0d8cba
Eric Sammons <esammons@redhat.com>
2012-02-08 07:43:33 -0500

    Started a basis for creating adhoc manifests.  Lots of work to be done here.

---
+++ api/manifest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import uuid
+import os
+import tempfile
+import zipfile
+
+class Manifest:
+   def generateUUID(self):
+      return uuid.uuid4()
+
+   def getFullExport(self):
+      tmpDir = tempfile.mkdtemp(suffix='', prefix='', dir=None)
+      basedir = os.path.abspath(tmpDir)
+
+

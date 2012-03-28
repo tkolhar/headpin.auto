4a68592a00edae9081e0558aa8a213ff31ac14a3
Eric Sammons <esammons@redhat.com>
2012-03-28 13:25:06 -0400

    Minor updates

--- cli_tests/test_cli_ping.py
+++ cli_tests/test_cli_ping.py
@@ -4,11 +4,13 @@ import pytest
 from unittestzero import Assert
 from commands.ping import Ping
 
+xfail = pytest.mark.xfail
+
 class TestPing:
     
     def test_ping(self):
         ping = Ping()
         
         out, err = ping.ping_error()
-        Assert.equal(err, "None")
+        Assert.equal(err, None)
         

3ca132b83d1b96fca7c7cca8db02b67640c8bdc3
Eric Sammons <esammons@redhat.com>
2012-05-16 18:36:09 -0400

    Updated test_roles_data_driver to support changes to create_new_role()

--- tests/test_roles_datadriven.py
+++ tests/test_roles_datadriven.py
@@ -40,9 +40,7 @@ class TestRolesDataDriven(object):
         
         home_page.tabs.click_tab("administration_tab")
         home_page.tabs.click_tab("roles_administration")
-        home_page.click_new()
         rolestab.create_new_role(role_name)
-        rolestab.save_role()
         
         rolestab.click_role_permissions()
             

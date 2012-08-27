ef55eed462c8cae2845850cb76d06917e0871836
aweiteka <aweiteka@redhat.com>
2012-08-27 10:38:21 -0400

    added one common locator: login

--- pages/locators.py
+++ pages/locators.py
@@ -5,13 +5,6 @@ from selenium.webdriver.common.by import By
 """
 Common locators for all products are contained within.
 """
-# principle nav by direct URL
-# in that case only forms and buttons need locators
 
-# options:
-# some_locator_by_id = (By.ID, 'someLocator')
-# some_locator_by_css = (By.CSS_SELECTOR, '#someLocator')
-# some_locator_by_xpath = (By.XPATH, "//div[@id='someLocator']")
-# some_elements_locator = (By.CSS_SELECTOR, 'li .someElementsLocator')
-
+login_locator = (By.NAME, "commit")
 

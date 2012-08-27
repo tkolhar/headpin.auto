a475ab6e7c28b3996871766cbfac2b12fe031e78
aweiteka <aweiteka@redhat.com>
2012-08-27 13:44:03 -0400

    added locators for account provider creation and connection test

--- pages/aeolus/locators.py
+++ pages/aeolus/locators.py
@@ -27,3 +27,22 @@ user_group_name_field = (By.ID, "user_group_name")
 user_group_description_field = (By.ID, "user_group_description")
 user_group_submit_locator = (By.ID, "user_group_submit")
 user_group_delete_locator = (By.ID, "delete")
+
+# new provider account fields
+prov_acct_details_locator = (By.ID, "details_accounts")
+prov_acct_new_account_field = (By.ID, "new_provider_account")
+prov_acct_name_field = (By.ID, "provider_account_label")
+prov_acct_access_key_field = (By.ID, "provider_account_credentials_hash_username")
+prov_acct_secret_access_key_field = (By.ID, "provider_account_credentials_hash_password")
+prov_acct_number_field = (By.ID, "provider_account_credentials_hash_account_id")
+#file input
+prov_acct_key_locator = (By.ID, "provider_account_credentials_hash_x509private")
+prov_acct_cert_locator = (By.ID, "provider_account_credentials_hash_x509public")
+prov_acct_prior_field = (By.ID, "provider_account_priority")
+prov_acct_quota_field = (By.ID, "quota_instances")
+prov_acct_save_locator = (By.ID, "save")
+prov_acct_delete_locator = (By.ID, "delete_button")
+
+# confirmation flash message locators
+success_confirmation_locator = (By.ID, "flashes")
+fail_confirmation_locator = (By.ID, "flash-subset")

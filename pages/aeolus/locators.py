f3f42930391b42cb273142dad041e208a1d9fefe
aweiteka <aweiteka@redhat.com>
2012-08-27 10:37:34 -0400

    added user and user group fields

--- pages/aeolus/locators.py
+++ pages/aeolus/locators.py
@@ -5,16 +5,25 @@ from selenium.webdriver.common.by import By
 """
 Locators for aeolus pages are contained within.
 """
-# principle nav by direct URL
-# in that case only forms and buttons need locators
 
-# options:
-# some_locator_by_id = (By.ID, 'someLocator')
-# some_locator_by_css = (By.CSS_SELECTOR, '#someLocator')
-# some_locator_by_xpath = (By.XPATH, "//div[@id='someLocator']")
-# some_elements_locator = (By.CSS_SELECTOR, 'li .someElementsLocator')
-
+# login
 username_text_field = (By.NAME, "login")
 password_text_field = (By.ID, "password-input")
 login_locator = (By.NAME, "commit")
 
+# new user fields
+user_first_name_field = (By.ID, "user_first_name")
+user_last_name_field = (By.ID, "user_last_name")
+user_email_field = (By.ID, "user_email")
+user_username_field = (By.ID, "user_login")
+user_password_field = (By.ID, "user_password")
+user_password_confirmation_field = (By.ID, "user_password_confirmation")
+user_quota_max_running_instances_field = (By.ID, "user_quota_attributes_maximum_running_instances")
+user_submit_locator = (By.ID, "user_submit")
+user_delete_locator = (By.CSS_SELECTOR, "input.button.danger")
+
+# new user group fields
+user_group_name_field = (By.ID, "user_group_name")
+user_group_description_field = (By.ID, "user_group_description")
+user_group_submit_locator = (By.ID, "user_group_submit")
+user_group_delete_locator = (By.ID, "delete")
